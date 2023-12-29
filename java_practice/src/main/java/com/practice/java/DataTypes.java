4dd333a73caf3a2e4a42bfc817860dd587351f0e
KalirajP <kalirajp94@gmail.com>
2023-12-29 18:27:29 +0530

    adding data type practice class with maven project structure

---
+++ java_practice/src/main/java/com/practice/java/DataTypes.java
@@ -0,0 +1,35 @@
+
+package com.practice.java;
+
+public class DataTypes {
+    
+    String str_var ="Product";
+    int num=5;
+    float myFloat=50.10f;
+    char myChar='h';
+    boolean myBoolean=true;
+    byte myByte=12;
+    double myDouble=15.00001;
+    long myLong;
+    short myShort;
+
+    
+
+    public static void main(String[] args) {
+
+        DataTypes obj= new DataTypes();
+        
+        System.out.println(obj.str_var);
+        System.out.println(obj.num);
+        System.out.println(obj.myFloat);
+        System.out.println(obj.myLong);
+        System.out.println(obj.myDouble);
+        System.out.println(obj.myChar);
+        System.out.println(obj.myBoolean);
+        System.out.println(obj.myByte);
+        System.out.println(obj.myShort);
+        
+
+    }
+
+}
